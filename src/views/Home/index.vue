861663fd88e36e8cb72c1fbd1280dfe4a12dc24d
范志鹏2002 <2710557309@qq.com>
2024-02-18 15:06:29 +0800

    整体结构拆分与分类实现

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,9 +1,19 @@
-<script>
-export default {};
+<script setup>
+import HomeCategory from "./components/HomeCategory.vue";
+import HomeBanner from "./components/HomeBanner.vue";
+import HomeHot from "./components/HomeHot.vue";
+import HomeNew from "./components/HomeNew.vue";
+import HomeProduct from "./components/HomeProduct.vue";
 </script>
 
 <template>
-  <div>我是HOME</div>
+  <div class="container">
+    <HomeCategory />
+    <HomeBanner />
+  </div>
+  <HomeNew />
+  <HomeHot />
+  <HomeProduct />
 </template>
 
 <style></style>
