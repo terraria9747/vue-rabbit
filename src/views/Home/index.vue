524a8e66720607249bd8543665b500e0d1a215b0
范志鹏2002 <2710557309@qq.com>
2024-02-18 15:49:39 +0800

    面板组件封装

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -4,6 +4,7 @@ import HomeBanner from "./components/HomeBanner.vue";
 import HomeHot from "./components/HomeHot.vue";
 import HomeNew from "./components/HomeNew.vue";
 import HomeProduct from "./components/HomeProduct.vue";
+import HomePannel from "./components/HomePannel.vue";
 </script>
 
 <template>
@@ -11,6 +12,12 @@ import HomeProduct from "./components/HomeProduct.vue";
     <HomeCategory />
     <HomeBanner />
   </div>
+  <HomePannel name="新鲜好物" sub-title="新鲜好物 好多商品">
+    <div>我是新鲜好物模块</div>
+  </HomePannel>
+  <HomePannel name="人气推荐" sub-title="人气推荐 好多商品">
+    <div>我是人气推荐模块</div>
+  </HomePannel>
   <HomeNew />
   <HomeHot />
   <HomeProduct />
