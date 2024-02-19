45719e1bc65f4d475e6eb275c1baa16c2cda430a
范志鹏2002 <2710557309@qq.com>
2024-02-19 23:27:14 +0800

    处理401问题

--- src/utils/http.js
+++ src/utils/http.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import { ElMessage } from "element-plus";
 import "element-plus/theme-chalk/el-message.css";
 import { useUserStore } from "@/stores/user"
+import router from "@/router";
 
 // 配置基础实例
 const http = axios.create({
@@ -26,7 +27,18 @@ http.interceptors.request.use(function (config) {
 http.interceptors.response.use(function (response) {
 	return response;
 }, function (error) {
+	// 错误提示
 	ElMessage({ type: "warning", message: error.response.data.message });
+
+	// token过期了
+	const userStore = useUserStore()
+	if (error.response.status === 401) {
+		// 清除用户信息
+		userStore.clearUserInfo()
+
+		// 跳转到登录页
+		router.replace("/login")
+	}
 	return Promise.reject(error);
 });
 
