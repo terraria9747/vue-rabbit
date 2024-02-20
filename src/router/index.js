a241258fda396095f378b268141b926912963dfd
范志鹏2002 <2710557309@qq.com>
2024-02-20 13:57:07 +0800

    列表购物车基础数据渲染

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ import Home from "@/views/Home/index.vue"
 import Category from "@/views/Category/index.vue"
 import SubCategory from "@/views/SubCategory/index.vue"
 import Detail from "@/views/Detail/index.vue"
+import CartList from "@/views/CartList/index.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -33,6 +34,11 @@ const router = createRouter({
           // 详情页路由
           path: "detail/:id",
           component: Detail
+        },
+        {
+          // 列表购物车
+          path: "cartlist",
+          component: CartList
         }
       ]
     },
