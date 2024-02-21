be2c05eba6a1ede8a6e2b757d46587c9b9f31b86
范志鹏2002 <2710557309@qq.com>
2024-02-21 11:45:02 +0800

    支付结果展示

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,7 @@ import Detail from "@/views/Detail/index.vue"
 import CartList from "@/views/CartList/index.vue"
 import Checkout from "@/views/Checkout/index.vue"
 import Pay from "@/views/Pay/index.vue"
+import Paycallback from "@/views/Paycallback/index.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -51,6 +52,11 @@ const router = createRouter({
           // 支付
           path: "pay",
           component: Pay
+        },
+        {
+          // 支付回调
+          path: "paycallback",
+          component: Paycallback
         }
       ]
     },
